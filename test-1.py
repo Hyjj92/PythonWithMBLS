85403adab5c894e42d6a94d79acde1b695f2244f
Hyjj92 <55699153+Hyjj92@users.noreply.github.com>
2019-09-28 14:29:05 +0800

    first upload my file
    第一次提交python的小练习

---
+++ test-1.py
@@ -0,0 +1,40 @@
+#使用type()检查变量的类型
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a)) # <class 'int'>
+print(type(b)) # <class 'float'>
+print(type(c)) # <class 'complex'>
+print(type(d)) # <class 'str'>
+print(type(e)) # <class 'bool'>
+
+"""
+比较、逻辑和算身份运算符的使用
+"""
+flag0 = 1 == 1
+flag1 = 3 > 2
+flag2 = 2 < 1
+flag3 = flag1 and flag2
+flag4 = flag1 or flag2
+flag5 = not (1 != 2)
+print('flag0 =', flag0) # flag0 = True
+print('flag1 =', flag1) # flag1 = True
+print('flag2 =', flag2) # flag2 = False
+print('flag3 =', flag3) # flag3 = False
+print('flag4 =', flag4) # flag4 = True
+print('flag5 =', flag5) # flag5 = False
+print(flag1 is True) # True
+print(flag2 is not False) # False
+
+
+"""
+输入年份 如果是闰年输出True 否则输出False
+
+"""
+
+year = int(input('请输入年份: '))
+
+is_leapYear = (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+print('这一年是闰年是：'+str(is_leapYear))
