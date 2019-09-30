9c16341d3f0eee944f9910797a46c3bce1f8d094
Hyjj92 <55699153+Hyjj92@users.noreply.github.com>
2019-09-30 10:59:50 +0800

    谨以此向共和国七十周年献礼
    用python绘制一面国旗。
    我和我的祖国一刻也不能分割，无论我走到哪里都流出一首赞歌，我歌唱每一座高山我歌唱每一条河，袅袅炊烟小小村落路上一道辙，我最亲爱的祖国我永远紧依着你的心窝，你用你那母亲的脉搏和我诉说，我的祖国和我像海和浪花一朵，浪是海的赤子海是那浪的依托，每当大海在微笑我就是笑的旋涡，我分担着海的忧愁分享海的欢乐，我最亲爱的祖国你是大海永不干涸，永远给我碧浪清波心中的歌。

---
+++ test2.py
@@ -0,0 +1,76 @@
+# author : momo
+import turtle
+# 中国国旗
+turtle.up()
+turtle.goto(-200, 200)
+turtle.down()
+turtle.begin_fill()
+turtle.fillcolor("red")
+turtle.pencolor("red")
+for i in range(2):
+    turtle.forward(280)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+turtle.end_fill()
+ 
+turtle.up()
+turtle.goto(-170, 145)
+turtle.down()
+turtle.begin_fill()
+turtle.fillcolor("yellow")
+turtle.pencolor("yellow")
+for x in range(5):
+    turtle.forward(50)
+    turtle.right(144)
+turtle.end_fill()
+ 
+turtle.up()
+turtle.goto(-100, 180)
+turtle.down()
+turtle.begin_fill()
+turtle.fillcolor("yellow")
+turtle.pencolor("yellow")
+for x in range(5):
+    turtle.forward(20)
+    turtle.right(144)
+turtle.end_fill()
+ 
+turtle.up()
+turtle.goto(-70, 160)
+turtle.down()
+turtle.begin_fill()
+turtle.fillcolor("yellow")
+turtle.pencolor("yellow")
+for x in range(5):
+    turtle.forward(20)
+    turtle.right(144)
+turtle.end_fill()
+ 
+turtle.up()
+turtle.goto(-70, 120)
+turtle.down()
+turtle.begin_fill()
+turtle.fillcolor("yellow")
+turtle.pencolor("yellow")
+for x in range(5):
+    turtle.forward(20)
+    turtle.right(144)
+turtle.end_fill()
+ 
+turtle.up()
+turtle.goto(-100, 100)
+turtle.down()
+turtle.begin_fill()
+turtle.fillcolor("yellow")
+turtle.pencolor("yellow")
+for x in range(5):
+    turtle.forward(20)
+    turtle.right(144)
+turtle.end_fill()
+ 
+ 
+turtle.hideturtle()
+# 隐藏小海龟
+# 维持面板
+turtle.done()
